fd8af5cf3a8455e76fe82d6885eb075cafe4ee54
jgalvez1776 <jgalvez1776@gmail.com>
2021-05-15 17:53:45 -0700

    Added basic file structure

---
+++ src/main/Battleship.java
@@ -0,0 +1,10 @@
+package src.main;
+
+public class Battleship { 
+
+    public static void main(String[] args) {
+        // Okay now we can start
+        System.out.println("This works");
+    }
+
+}
