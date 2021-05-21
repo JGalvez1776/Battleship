001bb5736c72657cde213271dec864c7de659815
jgalvez1776 <jgalvez1776@gmail.com>
2021-05-20 22:19:41 -0700

    Added helper functions

--- src/main/Battleship.java
+++ src/main/Battleship.java
@@ -3,8 +3,28 @@ package src.main;
 public class Battleship { 
 
     public static void main(String[] args) {
-        System.out.println("This works");
+        
+        if (args.length == 0) {
+            startDefaultGame();
+        }
+    }
 
+    private static void startDefaultGame() {
+        
+    }   
+
+    private static void startGame(Player[] players) {
+
+    }
+
+    public static Ship[] defaultShips() {
+        Ship[] ships = new Ship[5];
+        ships[0] = new Ship(2);
+        ships[1] = new Ship(3);
+        ships[2] = new Ship(3);
+        ships[3] = new Ship(4);
+        ships[4] = new Ship(5);
+        return ships;
     }
 
 }
