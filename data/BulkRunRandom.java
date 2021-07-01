5d238c2b8ee13a04b3e65dd9789a281d4445886b
jgalvez1776 <jgalvez1776@gmail.com>
2021-06-30 20:38:14 -0700

    Makes output to file

--- data/BulkRunRandom.java
+++ data/BulkRunRandom.java
@@ -1,12 +1,28 @@
 package data;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import src.main.*;
 
 public class BulkRunRandom {
 
 
     public static void main(String[] args) {
+        File file = new File("data\\sample.txt");
+        PrintStream stream;
+        try {
+            stream = new PrintStream(file);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return;
+        }
+        System.setOut(stream);
+
         for (int i = 0; i < 1; i++) {
             Battleship.main(new String[0]);
         }   
     }
+
 }
